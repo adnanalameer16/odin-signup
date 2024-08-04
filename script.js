3b2b86871e378899db99f24c602099a4f81d57ae
Adnan Al Ameer <adnanalameer8@gmail.com>
2024-08-05 02:59:25 +0530

    login added

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
 let button=document.querySelector(".btn");
+let button2=document.querySelector(".btn2");
+
+let login=document.querySelector(".linklog");
+let create=document.querySelector(".linkcreate");
 
 let input=document.querySelectorAll("input");
 
 button.addEventListener('click', check);
+button2.addEventListener('click', check);
+
+login.addEventListener('click', () => {
+    document.querySelector(".login").style.display="flex";
+    document.querySelector(".createACCT").style.display="none";
+    document.querySelector(".button2").style.display="block";
+    document.querySelector(".button").style.display="none";
+});
+
+create.addEventListener('click', () => {
+    document.querySelector(".login").style.display="none";
+    document.querySelector(".createACCT").style.display="flex";
+    document.querySelector(".button2").style.display="none";
+    document.querySelector(".button").style.display="block";
+});
 
 function check()
 {
     let flag=1;
     input.forEach(i => {
-        if(!i.checkValidity())
-        {
-            i.classList.add('invalid');
-            flag=0;
-        }
-        else{
-            i.classList.remove('invalid');
-            i.classList.add('valid');
-        }
+        const isVisible = i.offsetParent !== null; 
     
+        if (isVisible) {
+            if (!i.checkValidity()) {
+                i.classList.add('invalid');
+                i.classList.remove('valid');
+                flag = 0;
+            } else {
+                i.classList.remove('invalid');
+                i.classList.add('valid');
+            }
+        }
     });
 
     let password=document.querySelector("#password").value;
